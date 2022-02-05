aa91be79aa64ad776e9725ef42ac638ed7cc8614
Tayan Sinha <tayansinha@gmail.com>
2022-02-05 18:59:13 +0530

    done

--- python/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-.txt', 'x') as s:
-        #     l=s.write('')
-        # with open('emp_20.txt', 'x') as s:
-        #     l=s.write('')
-        # with open('emp_30.txt', 'x') as s:
-        #     l=s.write('')
